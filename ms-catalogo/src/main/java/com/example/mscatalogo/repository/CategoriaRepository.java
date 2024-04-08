6728cd74cbf47d3a53f456855de3f3aeaa834c4f
unknown <you@example.com>
2024-04-08 09:16:47 -0500

    hiram10

--- ms-catalogo/src/main/java/com/example/mscatalogo/repository/CategoriaRepository.java
+++ ms-catalogo/src/main/java/com/example/mscatalogo/repository/CategoriaRepository.java
@@ -1,7 +1,8 @@
 package com.example.mscatalogo.repository;
 
 import com.example.mscatalogo.entity.Categoria;
+import com.example.mscatalogo.entity.Productos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
 }
