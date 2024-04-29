2244aefaac1e858517513a56021e6369475e5ac9
unknown <you@example.com>
2024-04-29 10:38:10 -0500

    hdiram67

--- ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
@@ -13,7 +13,7 @@ public interface CatalogoFeign {
     @GetMapping("/{id}")
     @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallBackproductoListarPorIdCB")
 
-    ResponseEntity<ProductoDto> productoBuscarPorId(@PathVariable(required = true) Integer id);
+    public ResponseEntity<ProductoDto> productoBuscarPorId(@PathVariable(required = true) Integer id);
     default ResponseEntity<ProductoDto> fallBackproductoListarPorIdCB(Integer id, Exception e) {
         ProductoDto productoDto =new ProductoDto();
         productoDto.setId(9000000);
