ac35ff597b1fbbb58e7b91e6d35bddb6377cee4e
unknown <you@example.com>
2024-04-29 10:50:58 -0500

    hirampro18

--- ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
+++ ms-pedido/src/main/java/com/example/mspedido/feign/CatalogoFeign.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-/*
+
 @FeignClient(name = "ms-catalogo-service",path = "/producto")
 public interface CatalogoFeign {
     @GetMapping("/{id}")
@@ -17,18 +17,6 @@ public interface CatalogoFeign {
     default ResponseEntity<ProductoDto> fallBackproductoListarPorIdCB(Integer id, Exception e) {
         ProductoDto productoDto =new ProductoDto();
         productoDto.setId(9000000);
-        return ResponseEntity.ok(new ProductoDto());
-    }
-
-}*/
-@FeignClient(name = "ms-catalogo-service", path = "/producto")
-public interface CatalogoFeign {
-    @GetMapping("/{id}")
-    @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallbackProducto")
-    public ResponseEntity<ProductoDto> productoBuscarPorId(@PathVariable(required = true) Integer id) ;
-    default ResponseEntity<ProductoDto> fallbackProducto (Integer id, Exception e) {
-        ProductoDto productoDto = new ProductoDto();
-        productoDto.setId(900000);
         return ResponseEntity.ok(new ProductoDto());
     }
 
